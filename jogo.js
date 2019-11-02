d942fa1b319f9cf43d0fd32e643aea876cfb23ac
anderson3007 <57275174+anderson3007@users.noreply.github.com>
2019-11-01 22:39:31 -0300

    código interagindo o usuário

--- jogo.js
+++ jogo.js
@@ -1,116 +1,49 @@
-var x = 25;
-var y = 15;
-//1 - jogar
-//2 - regras
-//3 - sair
-var opcao = 1 
-var telaAtiva = 1;
+let x = 100;
+let y = 100;
+let x1 = 20
+let x2 = 30
+let x3 = 40
+let i =0;
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(900, 512);
+  fill(255, 0, 0);
 }
 
 function draw() {
+  i = i +0.2
+  background('100');
   
-  switch(telaAtiva) {
-    case 1:
-      menu();
-      break;
-    case 2:
-      regras();
-      break;  
-    case 3:
-      sair();
-      break; 
+    x1= x1+i;
+    x2= x2+i;
+    x3= x2+i;
+  
+  rect(x1, 30, 30, 55);
+  rect(x2, 130, 30, 55);
+  rect(x3, 230, 30, 55);
+  
+  
+  if (keyIsDown(LEFT_ARROW)) {
+    x -= 5;
   }
-}
 
-function sair() {
-   //R, G, B
-  background('#7FDBFF');
-   
-  fill('white');
-  stroke('black');
-  strokeWeight(1);
-  
-  textSize(28);
-  fill('black');
-  
-  text('saindo', 40, 55);
-  window.close();
-}
-
-
-function regras() {
-   //R, G, B
-  background('#7FDBFF');
-   
-  fill('white');
-  stroke('black');
-  strokeWeight(1);
-  
-  textSize(28);
-  fill('black');
-  
-  text('Aqui vao as regras', 40, 55);
-}
-
-function menu() {
-  //R, G, B
-  background('#7FDBFF');
-   
-  fill('white');
-  stroke('black');
-  strokeWeight(1);
-  rect(30, 20, 120, 55, 10);
-  rect(30, 90, 120, 55, 10);
-  rect(30, 160, 120, 55, 10);
-  
-  textSize(32);
-  fill('black');
-  
-  text('Jogar', 40, 55);
-  text('Regras', 40, 125);
-  text('Sair', 40, 195);
-  
-  noFill();
-  stroke(255, 0, 0);
-  strokeWeight(4);
-  rect(x, y, 130, 65);
-}
-
-function keyPressed() {
-  console.log(keyCode);
-  
-  if(telaAtiva==2 || telaAtiva==3)
-      telaAtiva=1;
-  
-  if (keyCode === ENTER) 
-      telaAtiva=opcao;
-     
-  if (keyCode === DOWN_ARROW) {
-    if(y<155) {
-       y+=70;
-       if(opcao==1)
-           opcao=2;
-       else 
-          if(opcao==2)
-             opcao=3; 
-    }
-    
-  } else if (keyCode === UP_ARROW) {
-    if(y>20) {
-       y-=70;
-       if(opcao==2) {
-         opcao=1;
-       } else { 
-         if(opcao==3) {
-           opcao=2;
-         }
-       }
-    }
-   
+  if (keyIsDown(RIGHT_ARROW)) {
+    x += 5;
   }
-  console.log("opcao: "+opcao);
-  console.log("telaAtiva: "+telaAtiva);
+
+  if (keyIsDown(UP_ARROW)) {
+    y -= 5;
+  }
+
+  if (keyIsDown(DOWN_ARROW)) {
+    y += 5;
+  }
+  
+  if(x3 >=920){
+    i =0;
+    x1=0;
+    x2=100;
+    x3=300;
+  }
+  ellipse(x, y, 50, 50);
 }
